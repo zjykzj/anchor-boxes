e4ef509b50330e08944ae04870558594c3af8751
zjkjzj <wy163zhuj@163.com>
2024-01-23 20:46:23 +0800

    perf(v5/gen_anchor.py): modify the impl of yolov5 autoanchor.py to generate and test anchor boxes

--- v5/autoanchor.py
+++ v5/autoanchor.py
@@ -26,10 +26,13 @@ def check_anchor_order(m):
 
 
 @TryExcept(f'{PREFIX}ERROR')
-def check_anchors(dataset, model, thr=4.0, imgsz=640):
+# def check_anchors(dataset, model, thr=4.0, imgsz=640):
+def check_anchors(dataset, m, thr=4.0, imgsz=640):
     # Check anchor fit to data, recompute if necessary
-    m = model.module.model[-1] if hasattr(model, 'module') else model.model[-1]  # Detect()
+    # m = model.module.model[-1] if hasattr(model, 'module') else model.model[-1]  # Detect()
+    # 基于原始图像最大边等比例缩放
     shapes = imgsz * dataset.shapes / dataset.shapes.max(1, keepdims=True)
+    # 在真值边界框原始宽高的基础上进行随机尺度增强
     scale = np.random.uniform(0.9, 1.1, size=(shapes.shape[0], 1))  # augment scale
     wh = torch.tensor(np.concatenate([l[:, 3:5] * s for s, l in zip(shapes * scale, dataset.labels)])).float()  # wh
 
@@ -51,6 +54,8 @@ def check_anchors(dataset, model, thr=4.0, imgsz=640):
         LOGGER.info(f'{s}Anchors are a poor fit to dataset ⚠️, attempting to improve...')
         na = m.anchors.numel() // 2  # number of anchors
         anchors = kmean_anchors(dataset, n=na, img_size=imgsz, thr=thr, gen=1000, verbose=False)
+        # LOGGER.info(f"anchors shape: {anchors.shape}")
+        # LOGGER.info(f"m.anchors shape: {m.anchors.shape}")
         new_bpr = metric(anchors)[0]
         if new_bpr > bpr:  # replace anchors
             anchors = torch.tensor(anchors, device=m.anchors.device).type_as(m.anchors)
@@ -108,11 +113,11 @@ def kmean_anchors(dataset='./data/coco128.yaml', n=9, img_size=640, thr=4.0, gen
             LOGGER.info(s[:-2])
         return k
 
-    if isinstance(dataset, str):  # *.yaml file
-        with open(dataset, errors='ignore') as f:
-            data_dict = yaml.safe_load(f)  # model dict
-        from utils.dataloaders import LoadImagesAndLabels
-        dataset = LoadImagesAndLabels(data_dict['train'], augment=True, rect=True)
+    # if isinstance(dataset, str):  # *.yaml file
+    #     with open(dataset, errors='ignore') as f:
+    #         data_dict = yaml.safe_load(f)  # model dict
+    #     from utils.dataloaders import LoadImagesAndLabels
+    #     dataset = LoadImagesAndLabels(data_dict['train'], augment=True, rect=True)
 
     # Get label wh
     shapes = img_size * dataset.shapes / dataset.shapes.max(1, keepdims=True)
--- v5/gen_anchors.py
+++ v5/gen_anchors.py
@@ -1,26 +1,49 @@
 # -*- coding: utf-8 -*-
 
 """
-@Time    : 2024/1/23 15:01
+@Time    : 2024/1/23 20:01
 @File    : gen_anchors.py
 @Author  : zj
 @Description: 
 """
 
-import numpy as np
+import os
+import cv2
+import glob
 import torch
 
-from autoanchor import check_anchor_order
+from tqdm import tqdm
+import numpy as np
+
+from autoanchor import check_anchor_order, check_anchors, kmean_anchors
 
 
 class Model:
 
     def __init__(self, anchors=None, stride=None):
-        self.anchors = torch.from_numpy(np.array(anchors, dtype=float))
-        self.stride = stride
+        """
+
+        :param anchors: [Nl, Na, 2], where Nl is the number of detection layers, Na is the number of anchors, and 2 is the width and height of each anchor
+        :param stride: [Nl], where Nl is the scaling factor of each detection layer relative to the input image
+        """
+        self.anchors = torch.from_numpy(np.array(anchors, dtype=float)).view(len(anchors), -1, 2)
+        self.stride = torch.from_numpy(np.array(stride, dtype=int))
+        assert len(self.anchors) == len(self.stride)
 
 
-if __name__ == '__main__':
+class Dataset:
+
+    def __init__(self, shapes=None, labels=None):
+        """
+        :param shapes: [N, 2], where N is the number of images and 2 is the width and height of each image
+        :param labels: [N, ...], where N is the number of images, each item in the list contains the annotation box width and height of the corresponding image
+        """
+        self.shapes = shapes
+        self.labels = labels
+        assert len(self.shapes) == len(self.labels)
+
+
+def test_model():
     anchors = np.array([
         [10, 13, 16, 30, 33, 23],
         [30, 61, 62, 45, 59, 119],
@@ -34,3 +57,56 @@ if __name__ == '__main__':
     check_anchor_order(m)
     print("anchors:", m.anchors)
     print("stride:", m.stride)
+
+
+def get_yolov5_data(root, name):
+    image_dir = os.path.join(root, name, "images")
+    assert os.path.isdir(image_dir), image_dir
+    label_dir = os.path.join(root, name, 'labels')
+    assert os.path.isdir(label_dir), label_dir
+    label_path_list = sorted(glob.glob(os.path.join(label_dir, '*.txt')))
+
+    shapes = list()
+    labels = list()
+
+    for label_path in tqdm(label_path_list):
+        # [[box_w, box_h], ]
+        # The coordinate size is relative to the width and height of the image
+        boxes = np.loadtxt(label_path, delimiter=' ', dtype=float)
+        if len(boxes) == 0:
+            continue
+        if len(boxes.shape) == 1:
+            boxes = [boxes]
+        # for label, xc, yc, box_w, box_h in boxes:
+        #     box_list.append([box_w, box_h])
+        labels.append(np.array(boxes))
+
+        image_name = os.path.basename(label_path).replace(".txt", ".jpg")
+        image_path = os.path.join(image_dir, image_name)
+        assert os.path.isfile(image_path), image_path
+        image = cv2.imread(image_path)
+        h, w = image.shape[:2]
+        shapes.append((w, h))
+
+    return np.array(shapes), labels
+
+
+def test_dataset():
+    shapes, labels = get_yolov5_data("../../datasets/", "voc2yolov5-train")
+    dataset = Dataset(shapes, labels)
+
+    anchors = np.array([
+        [10, 13, 16, 30, 33, 23],
+        [30, 61, 62, 45, 59, 119],
+        [116, 90, 156, 198, 373, 326]
+    ])
+    stride = [8, 16, 32]
+    m = Model(anchors=anchors, stride=stride)
+
+    check_anchors(dataset, m)
+    print('anchors:', m.anchors)
+
+
+if __name__ == '__main__':
+    test_dataset()
+    # test_model()
